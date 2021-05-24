8b7903eabadd3353ecd9cd4931f4e422985ef2cf
Owen Walpole <owen@walpole.dev>
2021-05-23 23:01:47 -0500

    Add columns iterator

--- src/blur/columns.rs
+++ src/blur/columns.rs
@@ -15,14 +15,87 @@ impl<T: Iterator> ColumnIter for T {}
 pub trait GridSlice<T> {
     fn column(&self, x: usize, width: usize) -> SliceColumn<'_, T>;
     fn column_mut(&mut self, x: usize, width: usize) -> SliceColumnMut<'_, T>;
+    fn columns(&self, width: usize) -> Columns<'_, T>;
+    unsafe fn columns_mut(&mut self, width: usize) -> ColumnsMut<'_, T>;
 }
 
 impl<T> GridSlice<T> for [T] {
     fn column(&self, x: usize, width: usize) -> SliceColumn<'_, T> {
         self.iter().column(x, width)
     }
-    
+
     fn column_mut(&mut self, x: usize, width: usize) -> SliceColumnMut<'_, T> {
         self.iter_mut().column(x, width)
+    }
+
+    fn columns(&self, width: usize) -> Columns<'_, T> {
+        Columns::new(self, width)
+    }
+
+    unsafe fn columns_mut(&mut self, width: usize) -> ColumnsMut<'_, T> {
+        ColumnsMut::new(self, width)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Columns<'a, T: 'a> {
+    v: &'a [T],
+    width: usize,
+    x: usize,
+}
+
+#[derive(Debug)]
+pub struct ColumnsMut<'a, T: 'a> {
+    v: &'a mut [T],
+    width: usize,
+    x: usize,
+}
+
+impl<'a, T: 'a> Columns<'a, T> {
+    pub const fn new(slice: &'a [T], width: usize) -> Self {
+        Self {
+            v: slice,
+            width,
+            x: 0,
+        }
+    }
+}
+
+impl<'a, T: 'a> ColumnsMut<'a, T> {
+    pub unsafe fn new(slice: &'a mut [T], width: usize) -> Self {
+        Self {
+            v: slice,
+            width,
+            x: 0,
+        }
+    }
+}
+
+impl<'a, T> Iterator for Columns<'a, T> {
+    type Item = SliceColumn<'a, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v.is_empty() || self.x == self.width {
+            None
+        } else {
+            self.x += 1;
+            Some(self.v.column(self.x - 1, self.width))
+        }
+    }
+}
+
+impl<'long, T> Iterator for ColumnsMut<'long, T> {
+    type Item = SliceColumnMut<'long, T>;
+
+    fn next<'short>(&'short mut self) -> Option<Self::Item> {
+        use std::mem::transmute;
+        if self.v.is_empty() || self.x == self.width {
+            None
+        } else {
+            self.x += 1;
+            let ret = self.v.column_mut(self.x - 1, self.width);
+            let ret = unsafe { transmute::<SliceColumnMut<'short, T>, Self::Item>(ret) };
+            Some(ret)
+        }
     }
 }
